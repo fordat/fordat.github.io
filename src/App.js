9fb58414e1843d5447849db5dcbbddf856737f50
fordat <fordatwater@gmail.com>
2021-04-17 13:23:35 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <div className="App-box">
-        <h1 className="content">My React App</h1>
+        <h1 className="content">Ford Atwater</h1>
         <h2 className="content">Author: Ford Atwater</h2>
       </div>
     </div>
