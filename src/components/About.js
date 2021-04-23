390cf611eb527fd942754e9bf44025068fea6997
fordat <fordatwater@gmail.com>
2021-04-22 23:08:52 -0700

    lavender

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 class About extends React.Component {
+    // state = {
+    //     color: "green"
+    // }
+
+    // appbox = document.querySelector(".App-box");
+    // mainnav = document.querySelector(".main-nav");
+    // navlink = document.querySelector(".nav-link");
+    // bodycolor = document.querySelector(".App-box");
+
+    // changeable = [];
+
+    // changeColors() {
+    //     console.log("ayyyyyyy");
+    //     document.body.classList.add('body-green');
+
+    // }
+
     render() {
         return (
             <div>
                 <p>About component!</p>
             </div>
+
         )
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 class Navbar extends React.Component {
     render() {
         return (
-            <div class="nav-wrapper">
-                <nav class="main-nav">
+            <div className="nav-wrapper">
+                <nav className="main-nav">
                     <Link to="/work" className="nav-link">Work</Link>
                     <Link to="/painting" className="nav-link">Painting</Link>
-                    <Link to="/">About</Link>
+                    <Link to="/" className="nav-link">About</Link>
                 </nav>
             </div>
         )
