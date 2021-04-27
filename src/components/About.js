408bcdaa75f47c4b953018380fa845b3e09f4414
fordat <fordatwater@gmail.com>
2021-04-26 20:33:02 -0700

    yep

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ class About extends React.Component {
                 <h3 className="info">About Me</h3>
                 <p className="info">I'm a researcher and developer working out of Vancouver, BC.</p>
                 <p className="info">Currently, I'm a laboratory manager at the <a className="wlink" href="https://viscoglab.psych.ubc.ca/">UBC Visual Cognition Lab</a>, where I study information visualization and contribute to our codebase.  Previously, I worked as a software developer for the Department of Psychology's <a className="wlink" href="https://demo.tapestry-tool.com/tapestry/walkthrough/#/nodes/4827">Tapestry Tool</a>.  I received my B.Sc. in Cognitive Systems with a focus in Computer Science from UBC in 2020.</p>
-                <p className="info">In my free time, I like to cook, paint, swim, go on walks, and work on my bicycle.</p>
+                <p className="info">In my free time, I like to cook, paint, play the piano, go on walks, and work on my bicycle.</p>
             </div>
         )
     }
