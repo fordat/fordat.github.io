cb406d045240145fbc0ee0b78d3ac0f09808a467
fordat <fordatwater@gmail.com>
2022-01-26 19:45:08 -0800

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ class About extends React.Component {
             <div className="info-wrapper">
                 <h3 className="info">About Me</h3>
                 <p className="info">I'm a software developer who lives and works in Vancouver, BC.</p>
-                <p className="info">Currently, I'm at Fyrefish Technologies, working on designs and code for new products.  Previously, I was a laboratory manager, research, and developer at the <a className="wlink" href="https://viscoglab.psych.ubc.ca/">UBC Visual Cognition Lab</a>, where I got to work on problems relating to cognitive science, perception, and data visualization.</p>
-                <p className="info">In my free time, I like to cook, eat, paint, garden, swim, and ride my bicycle.</p>
+                <p className="info">Currently, I'm at Fyrefish Technologies, working on designs and code for new products.  Previously, I was a laboratory manager, researcher, and developer at the <a className="wlink" href="https://viscoglab.psych.ubc.ca/">UBC Visual Cognition Lab</a>, where I got to work on problems relating to cognitive science, perception, and data visualization.</p>
+                <p className="info">In my free time, I like to cook, eat, swim, play board games, and mess around with my bicycle.</p>
             </div>
         )
     }
